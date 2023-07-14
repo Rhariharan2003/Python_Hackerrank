4095088c9f84011764455fffe712c437d07a888b
Rhariharan2003 <136108485+Rhariharan2003@users.noreply.github.com>
2023-07-14 19:25:04 +0530

    Create 5.Loop.py

---
+++ Introduction/5.Loop.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+for i in range(0,n):
+    print(i**2)
+
+#Problem Link -> https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
